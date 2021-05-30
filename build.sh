1f2bbdfa63d0a917cfaa3387c3ae9cb2fd196b2c
Zekiah-A <zekiahamoako@outlook.com>
2021-05-31 00:44:13 +0100

    Fixed compiler and Object-oriented rects

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Building files"
-#gcc Main.cpp -o blender_ui -lGL -lGLU -lglut
-if  gcc main.cpp main.hpp rect.cpp rect.hpp -o blender_ui -lGL -lGLU -lglut -lX11; then
+#gcc main.cpp main.hpp rect.cpp rect.hpp -o blender_ui -lGL -lGLU -lglut # doesn't work with gcc, wtf
+if  g++ main.cpp main.hpp rect.cpp rect.hpp -o blender_ui -lGL -lGLU -lglut; then
     echo "Project sucessful built as 'blender_ui', running."
 else
     echo "Build failed."
--- main.cpp
+++ main.cpp
@@ -11,31 +11,31 @@
 void Main::draw_button(float locx, float locy) //add position
 {
 	glColor3f(1, 1, 1);
-	Rect *r1 = new Rect(0 + locx, locy, 0.1, 0.05, 1);
+	new Rect(0 + locx, locy, 0.1, 0.05, 1);
 	glColor3f(0.5, 0.5, 0.5);
-	Rect *r2 = new Rect(0 + locx, locy, 0.09, 0.04, 1);
+	new Rect(0 + locx, locy, 0.09, 0.04, 1);
 	glColor3f(0.7, 0.7, 0.7);
-	Rect *r3 = new Rect(-0.025 + locx, locy, 0.04, 0.04, 1);
+	new Rect(-0.025 + locx, locy, 0.04, 0.04, 1);
 }
 
 void Main::draw_checkbox(float locx, float locy, bool ischecked)
 {
-	Rect *r1 = new Rect(locx, locy, 0.06, 0.06, 1);
+	new Rect(locx, locy, 0.06, 0.06, 1);
 
 	if (ischecked == true)
 		glColor3f(0, 1, 0);
 	else
 		glColor3f(1, 0, 0);
-	Rect *r2 = new Rect(locx, locy, 0.05, 0.05, 1);
+	new Rect(locx, locy, 0.05, 0.05, 1);
 	glColor3f(1, 1, 1);
 }
 
 void Main::draw_textbox(float locx, float locy)
 {       //outer design rect
-	Rect *r1 = new Rect(locx, locy, 0.2, 0.05, 1);
+	new Rect(locx, locy, 0.2, 0.05, 1);
 	//inner text entry box
 	glColor3f(0.7, 0.7, 0.7);
-	Rect *r2 = new Rect(locx, locy, 0.19, 0.04, 1);	
+	new Rect(locx, locy, 0.19, 0.04, 1);	
 }
 
 void change_size(int w, int h)
@@ -67,20 +67,20 @@ void render_screen(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	new Rect(0.5, 0.5, 0.5, 0.5, 1);
-	//Rect *r2 = new Rect(-0.5, -0.5, 0.5, 0.5, 1);
-	//Rect *r3 = new Rect(0.5, -0.5, 0.5, 0.5, 1);
-	//Rect *r4 = new Rect(-0.5, 0.5, 0.5, 0.5, 1);
+	Rect *r1 = new Rect(0.5, 0.5, 0.5, 0.5, 1);
+	Rect *r2 = new Rect(-0.5, -0.5, 0.5, 0.5, 1);
+	Rect *r3 = new Rect(0.5, -0.5, 0.5, 0.5, 1);
+	Rect *r4 = new Rect(-0.5, 0.5, 0.5, 0.5, 1);
 
-	//program.draw_button(0, 0.2);
-	//program.draw_button(0.2, 0);
-	//program.draw_button(0, -0.2);
-	//program.draw_button(-0.2, 0);
+	program.draw_button(0, 0.2);
+	program.draw_button(0.2, 0);
+	program.draw_button(0, -0.2); //TODO: these need classes too
+	program.draw_button(-0.2, 0);
 	
-	//program.draw_checkbox(0.1, 0, false);
-	//program.draw_checkbox(-0.1, 0, true);
+	program.draw_checkbox(0.1, 0, false);
+	program.draw_checkbox(-0.1, 0, true);
 	
-	//program.draw_textbox(0, 0.1);
+	program.draw_textbox(0, 0.1);
 
 	glutSwapBuffers();
 }
