0594be46309b5b0b57a5eb8a84d389ad0bc3fc9d
Zekiah-A <zekiahamoako@outlook.com>
2021-05-31 21:48:25 +0100

    Fixed minor annoyance in build script

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 echo "Building files"
 #gcc main.cpp main.hpp rect.cpp rect.hpp -o blender_ui -lGL -lGLU -lglut # doesn't work with gcc 
 if  g++ main.cpp main.hpp rect.cpp rect.hpp checkbox.cpp checkbox.hpp -o blender_ui button.cpp button.hpp -lGL -lGLU -lglut; then # turn up verbosity
-    echo -e "\e[1;32m Example project sucessfuly built as 'blender_ui', running. \e[0m"
+    echo -e "\e[1;32mExample project sucessfuly built as 'blender_ui', running. \e[0m"
 else
-    echo -e "\e[1;31m Build failed. \e[0m"
+    echo -e "\e[1;31mBuild failed. \e[0m"
 fi
 chmod +x blender_ui
 ./blender_ui
